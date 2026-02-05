0d52a572e9280ddf0305c998d87c23fa65f82a33
mvaayu <mailvaayu1402@gmail.com>
2026-02-05 21:27:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-print("🚀 Bot is alive!")
+import yfinance as yf
+
+
+def fetch_market_data():
+    nifty = yf.Ticker("^NSEI")
+
+    data = nifty.history(period="1d")
+
+    close = round(data["Close"].iloc[-1], 2)
+    open_price = round(data["Open"].iloc[-1], 2)
+
+    change = round(close - open_price, 2)
+    percent = round((change / open_price) * 100, 2)
+
+    print("📊 Market Recap")
+    print(f"NIFTY Close: {close}")
+    print(f"Change: {change} ({percent}%)")
+
 
 def main():
-    print("Running automation successfully...")
+    print("🚀 Bot started")
+    fetch_market_data()
+
 
 if __name__ == "__main__":
     main()
